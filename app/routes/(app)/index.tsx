062af8992fbf67223a8167f5e61ab43f6c4488a1
Yu Yin <yxonic@gmail.com>
2025-03-12 02:59:38 +0800

    add editor

--- app/routes/(app)/index.tsx
+++ app/routes/(app)/index.tsx
@@ -1,6 +1,5 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { Button } from "app/components/ui/button";
-import logo from "@/assets/logo.svg";
+import { createFileRoute } from "@tanstack/react-router";
+import Tiptap from "@/components/editor/Tiptap";
 
 export const Route = createFileRoute("/(app)/")({
   component: Home,
@@ -8,40 +7,8 @@ export const Route = createFileRoute("/(app)/")({
 
 function Home() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>app/routes/index.tsx</code> and save to reload.
-        </p>
-        <div className="mt-8 grid grid-cols-2 gap-4">
-          <Button asChild>
-            <a
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </Button>
-          <Button asChild>
-            <a
-              href="https://tanstack.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn TanStack
-            </a>
-          </Button>
-        </div>
-        <Link to="/about" className="mt-8 underline">
-          about
-        </Link>
-      </header>
+    <div className="post py-12 px-48">
+      <Tiptap />
     </div>
   );
 }
